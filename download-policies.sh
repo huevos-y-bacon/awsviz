14b3871a5b4385b15e4fde41236804310fed83cb
huevos-y-bacon <perfectlydestroyed@gmail.com>
2024-07-11 10:18:46 +0100

    Add parallelisation and filtering options

--- download-policies.sh
+++ download-policies.sh
@@ -1,7 +1,61 @@
 #!/bin/bash
+
+function usage() {
+  echo "Usage: $0 [--attached] [--local]" >&2
+  echo "Options:" >&2
+  echo "  --attached, -A  List only attached policies" >&2
+  echo "  --local, -L     List only local policies" >&2
+  echo "  --help, -H      This" >&2
+  exit 0
+}
+
+function fetch_policy() {
+  policy_name=$(basename "$arn")
+  echo "ARN Policy: $arn"
+  # Retrieve the default version of the policy
+  [[ -n $DEBUG ]] && echo "Fetching default version for policy: $arn"
+  default_version=$(aws iam get-policy --policy-arn "$arn" --query 'Policy.DefaultVersionId' --output text)
+  if [ $? -ne 0 ] || [ -z "$default_version" ]; then
+    echo "Error retrieving default version for policy: $arn" >&2
+    return
+  fi
+  [[ -n $DEBUG ]] && echo "Default version for $policy_name is $default_version"
+  # Retrieve the policy version document
+  [[ -n $DEBUG ]] && echo "Fetching policy JSON file from ARN: $arn"
+  policy_json=$(aws iam get-policy-version --policy-arn "$arn" --version-id "$default_version" --query 'PolicyVersion.Document' --output json)
+  if [ $? -ne 0 ]; then
+    echo "Error retrieving policy version document for policy: $arn with version: $default_version" >&2
+    return
+  fi
+  [[ -n $DEBUG ]] && {
+    # Print JSON output
+    echo "Policy JSON for $arn:"
+    echo "$policy_json"
+  }
+  # Save the policy JSON
+  echo "$policy_json" > "policies/${policy_name}.json"
+  echo "Saved policy document for $policy_name"
+}
 # Step 1: List all policies and save their ARNs to a file, one per line
 echo "Listing all policy ARNs..."
-aws iam list-policies --query 'Policies[*].Arn' --output text | tr '\t' '\n' > policy_arns.txt
+
+# Apply filter
+unset FILTER
+if [[ "$*" == *"--attached"* ]] || [[ "$*" == *"-A"* ]]; then
+  FILTER="${FILTER} --only-attached"
+  attached=Attached # Append to ZIP file name
+  echo "Filtering attached policies"
+fi
+if [[ "$*" == *"--local"* ]] || [[ "$*" == *"-L"* ]]; then
+  FILTER="${FILTER} --scope Local"
+  local=Local # Append to ZIP file name
+  echo "Filtering local (Customer Managed) policies"
+fi
+if [[ "$*" == *"--help"* ]] || [[ "$*" == *"-H"* ]]; then
+  usage
+fi
+
+aws iam list-policies ${FILTER} --query 'Policies[*].Arn' --output text | tr '\t' '\n' > policy_arns.txt
 if [ $? -ne 0 ]; then
   echo "Error listing policies"
   exit 1
@@ -11,37 +65,20 @@ mkdir -p policies
 # Step 3: Retrieve each policy document
 while IFS= read -r arn; do
   if [ -n "$arn" ]; then
-    policy_name=$(basename "$arn")
-    echo "ARN Policy: $arn"
-    # Retrieve the default version of the policy
-    echo "Fetching default version for policy: $arn"
-    default_version=$(aws iam get-policy --policy-arn "$arn" --query 'Policy.DefaultVersionId' --output text)
-    if [ $? -ne 0 ] || [ -z "$default_version" ]; then
-      echo "Error retrieving default version for policy: $arn" >&2
-      continue
-    fi
-    echo "Default version for $policy_name is $default_version"
-    # Retrieve the policy version document
-    echo "Fetching policy JSON file from ARN: $arn"
-    policy_json=$(aws iam get-policy-version --policy-arn "$arn" --version-id "$default_version" --query 'PolicyVersion.Document' --output json)
-    if [ $? -ne 0 ]; then
-      echo "Error retrieving policy version document for policy: $arn with version: $default_version" >&2
-      continue
-    fi
-    # Print JSON output
-    echo "Policy JSON for $arn:"
-    echo "$policy_json"
-    # Save the policy JSON
-    echo "$policy_json" > "policies/${policy_name}.json"
-    echo "Saved policy document for $policy_name"
+    fetch_policy &
   else
     echo "Empty ARN encountered, skipping..." >&2
   fi
 done < policy_arns.txt
+wait && echo "Background jobs completed"
+
 # Step 4: Create a zip file containing all policy documents
 echo "Creating zip file of all policy documents..."
-zip -r policies.zip policies
+
+ZIP_NAME=policies${attached}${local}.zip
+zip -r $ZIP_NAME policies
 # Optional: Cleanup
 echo "Cleaning up..."
 rm policy_arns.txt
+rm -rf policies
 echo "Done."
